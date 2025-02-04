b411136fa177a0fc0584db89f1ac207b69b22119
Muhammad-Mamoon-Kaleem <mamoonkaleem199@gmail.com>
2025-02-05 00:21:12 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { lazy, Suspense } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import Home from './pages/Home'
-import { Route, Routes } from 'react-router-dom'
-import AllBlogs from './pages/AllBlogs'
-import BlogDetail from './pages/BlogDetail'
-import BlogForm from './pages/BlogForm'
-import Login from './pages/LoginSignupForm'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
-import MyFavourite from './pages/MyFavourite'
 
-
+// Lazy Loading Components
+const Home = lazy(() => import('./pages/Home'));
+const AllBlogs = lazy(() => import('./pages/AllBlogs'));
+const BlogDetail = lazy(() => import('./pages/BlogDetail.jsx'));
+const BlogForm = lazy(() => import('./pages/BlogForm.jsx'));
+const Login = lazy(() => import('./pages/LoginSignupForm.jsx'));
+const MyFavourite = lazy(() => import('./pages/MyFavourite.jsx'));
 
 function App() {
-
-
   return (
-    <>
-
+    <div className="flex flex-col min-h-screen">  {/* Full height container */}
       <ToastContainer />
       <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/all' element={<AllBlogs />} />
-        <Route path='/allblogs/:category' element={<AllBlogs />} />
-         <Route path='/myfavourite' element={<MyFavourite/>} />
-        <Route path='/blogdetail/:id' element={<BlogDetail />} />
-        <Route path='/login' element={<Login />} />
-      </Routes>
-
+      
+      <main className="flex-grow">  {/* This pushes footer to the bottom */}
+        <Suspense fallback={<div className="text-center py-5">Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/all" element={<AllBlogs />} />
+            <Route path="/allblogs/:category" element={<AllBlogs />} />
+            <Route path="/myfavourite" element={<MyFavourite />} />
+            <Route path="/blogdetail/:id" element={<BlogDetail />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Suspense>
+      </main>
 
       <Footer />
-
-    </>
+    </div>
   )
 }
 
-export default App
+export default App;
